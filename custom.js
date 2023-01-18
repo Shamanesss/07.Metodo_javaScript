9a973c3baf6682ee42a3cea80fb99ed0f77c7668
Shamanesss <mentulbenodorna@gmail.com>
2023-01-18 21:03:38 +0100

    metodos javaScript

---
+++ custom.js
@@ -0,0 +1,55 @@
+
+let cadena = "Vamos a medir la longitud de esta frase";
+console.log(cadena.length);
+
+let grupoB = ["Edurne","Gorka","Maria","Xabier"];
+console.log(grupoB.length);
+
+let nodecir = "Vale";
+console.log(nodecir.repeat(4));
+
+let lugar = "Este es el lugar donde esta la palabra";
+console.log(lugar.indexOf("lugar") ) ;  
+console.log(lugar.indexOf("palabro") ) ;
+
+let buscar = "Los virus informaticos son una leyenda urbana";
+console.log(buscar.includes('virus'));
+console.log(buscar.includes('LEYENDA'));
+console.log(buscar.includes('informaticos', 11));
+
+let arrBuscar = ['virus', 'gusanos','troyanos','spam'];
+console.log(arrBuscar.includes('spam'));
+console.log(arrBuscar.includes('cookies'));
+
+let caracter ="Cuidado con el perro"
+console.log(caracter.charAt(15));
+console.log(caracter.charAt(16));
+console.log(caracter.charAt(17));
+console.log(caracter.charAt(18));
+console.log(caracter.charAt(19));
+
+console.log(caracter[0])
+console.log(caracter[1])
+console.log(caracter[2])
+console.log(caracter[3])
+console.log(caracter[4])
+console.log(caracter[5])
+console.log(caracter[6])
+
+let espacios = "         Aqui hay muchos espacios     ";
+console.log(espacios.trim());
+console.log(espacios.trimEnd());
+console.log(espacios.trimStart());
+
+
+
+let cambiar = "JavaScript"
+console.log(cambiar.replace("ava","-"))
+
+
+
+
+
+
+
+    
