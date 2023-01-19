501aa104f2d824bf7d8a7a097ab54502008ceaf8
Shamanesss <mentulbenodorna@gmail.com>
2023-01-20 00:04:43 +0100

    añadido subsstring, slice,tolowercase

--- custom.js
+++ custom.js
@@ -9,13 +9,16 @@ let nodecir = "Vale";
 console.log(nodecir.repeat(4));
 
 let lugar = "Este es el lugar donde esta la palabra";
-console.log(lugar.indexOf("lugar") ) ;  
+console.log(lugar.indexOf("esta") ) ;  
 console.log(lugar.indexOf("palabro") ) ;
+console.log(lugar.indexOf("e", 24));
+
+
 
 let buscar = "Los virus informaticos son una leyenda urbana";
 console.log(buscar.includes('virus'));
 console.log(buscar.includes('LEYENDA'));
-console.log(buscar.includes('informaticos', 11));
+console.log(buscar.includes('virus', 11));
 
 let arrBuscar = ['virus', 'gusanos','troyanos','spam'];
 console.log(arrBuscar.includes('spam'));
@@ -46,6 +49,28 @@ console.log(espacios.trimStart());
 let cambiar = "JavaScript"
 console.log(cambiar.replace("ava","-"))
 
+
+let frase = "Los ordenadores dominaran el MUNDO"
+console.log(frase.toLowerCase(frase))
+
+
+console.log(frase.toUpperCase(frase))
+
+let trozo ="Tener cuidado con los programadores que llevan destornillador."
+console.log(trozo.slice(6, 35))
+console.log(trozo.slice(-15))
+console.log(trozo.slice(47))
+
+let texto = "Internet es lo unico que se cae y nadie se rie";
+
+console.log(texto.substr(5));    
+console.log(texto.substr(15,5)); 
+
+let text = "Internet es lo unico que se cae y nadie se rie";
+console.log(text.substring(5));
+console.log(text.substring(5,15));
+console.log(text.substring(15,5))
+console.log(text.substring(15,20));
 
 
 
